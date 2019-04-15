bf6533b6809d90ae693551ee96e58c617f0dc8a2
Seng Ye Xiong <sengye.xiong@gmail.com>
2019-04-14 23:47:35 -0500

    Adding Remaining Views

--- sql/views.sql
+++ sql/views.sql
@@ -1,26 +1,43 @@
--- All_Ents
+CREATE VIEW  All_Ents AS
 select entity_name from entity;
 
--- All_Atts
+CREATE VIEW  All_Atts AS
 select attribute_name from attribute;
 
--- All_Keys
+CREATE VIEW  All_Keys AS
 select ac.table_name, acc.column_name
 from all_constraints ac
 join all_cons_columns  acc on ac.constraint_name = acc.constraint_name
 where ac.constraint_type = 'P' 
 order by ac.table_name;
 
--- All_Rels
+CREATE VIEW  All_Rels AS
 select relationship_name from relationship;
 
--- EntAtt
+CREATE VIEW  EntAtt AS
 select entity_name, attribute_name from has_attrbiute;
 
--- RelEnt
+CREATE VIEW  RelEnt AS
 select relationship_name, entity_name_1, entity_name_2 from relationship;
 
--- RelAtt
+
+CREATE VIEW  RelAtt AS
 select * from has_relationship_attribute;
 
--- AttDateType
+CREATE VIEW  AttDateType AS
+select attribute_name, data_type from attribute;
+
+
+CREATE VIEW  EntKey AS
+select e.entity_name, LISTAGG(a.attribute_name, ', ')
+WITHIN GROUP (ORDER BY a.attribute_name) "keys"
+from entity e
+join has_attrbiute hs on hs.entity_name = e.entity_name
+join attribute a on hs.attribute_name = a.attribute_name
+where a.attribute_type = 'primary'
+group by e.entity_name;
+
+CREATE VIEW  KeyAttr AS
+select ae.attribute_name, ak.attribute_name as "attribute_key"
+from attribute ae
+join attribute ak on ae.attribute_name = ak.parent_attribute;
